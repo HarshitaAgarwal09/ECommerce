237f694ba24b6edb6750323f8f9aa85d3528b0b9
HarshitaAgarwal09 <harshitaagarwal98@yahoo.com>
2019-02-10 23:43:19 +0530

    Cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,52 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../item';
 import { ActivatedRoute } from '@angular/router';
-import {IProduct} from '../product';
-import {ProductsService} from '../service/products.service';
+import { IProduct } from '../product';
+import { ProductsService } from '../service/products.service';
 
 @Component({
-
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
+
 export class CartComponent implements OnInit {
 	items: Item[] = [];
-	total :number = 0;
-	item:Item;
-  	id:number;
- 	
+	total: number = 0;
+
   constructor(private activatedRoute: ActivatedRoute,
-  		      private ps:ProductsService) { }
+  		      private ps: ProductsService) { }
   ngOnInit() { 
   	this.activatedRoute.params.subscribe(params =>{
-  		this.id = params['id'];	
+		let index:number = -1;
+  		var id:number = params['id'];
+  		let cart = [];		
+  		var item:Item =	{		
+  		  		product: this.ps.getProductById(id),
+				quantity: 1
+			}
+		if(localStorage.getItem('cart')!=null){
+  			JSON.parse(localStorage.getItem('cart'));
+   			index = -1;
+  			for(var i = 0 ; i < cart.length ; i++){
+  				if(cart[i].product.id == id){
+  					index = i;
+  				}
+  			}	
+  		}
+  		else{}
+  		if(index == -1){ 
+  			cart.push(item);
+  		}
+   		else{
+  			item = cart[index];
+  			item.quantity+=1;
+  			cart[index]=item;
+  		}	
+  		console.log(cart);
+  		localStorage.setItem("cart", JSON.stringify(cart))
   		this.loadCart();
   	})
   }
-
-  index=-1;
   
   loadCart(){
-  	for(var i = 0 ; i < this.items.length ; i++){
-  		if(this.items[i].product.id == this.id){
-  			this.index = i;
-  		}
-  	}
-
-  	if(this.index == -1){
-  		this.items.push({
-  				product: this.ps.getProductById(this.id),
-				quantity: 1
+ /* 	this.total = 0;
+		this.items = [];
+		let cart = JSON.parse(localStorage.getItem('cart'));
+		for (var i = 0; i < cart.length; i++) {
+			let item = JSON.parse(cart[i]);
+			this.items.push({
+				product: item.product,
+				quantity: item.quantity
 			});
-  		this.index=this.items.length-1;
-  	}
-  	else {
-  		this.items[this.index].quantity+=1;
-  	}
-  	this.total+=this.items[this.index].product.price;
-  	this.index = -1;
-  	console.log(this.total);
-
+			this.total += item.product.price * item.quantity;
+		}*/
   }
 }
